727a4b0e3a6fad2816787a9df3019fb8695e3278
Pamela Costa <pamelacosta.dev@gmail.com>
2020-12-10 19:57:56 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "12345",
+        nome: "Callie Torres",
+        partido: "ARISONA",
+        fotos: [{ url: "callie.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "67890",
+        nome: "Mark Sloan",
+        partido: "SLOAN",
+        fotos: [{ url: "mark.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "17",
+        nome: "Meredith Grey",
+        partido: "PSDB",
+        vice: "Cristina Yang",
+        fotos: [
+          { url: "meredith.png", legenda: "Prefeito" },
+          { url: "cristina.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "16",
+        nome: "Richard Webber",
+        partido: "PSB",
+        vice: "Miranda Bailey",
+        fotos: [
+          { url: "richard.jpg", legenda: "Prefeito" },
+          { url: "miranda.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
